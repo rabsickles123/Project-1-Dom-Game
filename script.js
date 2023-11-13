4e3f1f29ad3db1c52b0a15c4e3fc1984bebe7f21
Rabsickles91 <grewalrabian@gmail.com>
2023-11-13 14:39:23 -0500

    tenth commit

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ let paperChoice = document.querySelector("#paper")
 let scissorsChoice = document.querySelector("#scissors")
 let showDown_div = document.querySelector(".showdown")
 let resetButton = document.querySelector(".reset")
+let roundsPlayed = 0;
+let maxRounds = 7;
 let userChoice; 
 let computerChoice;
 
@@ -20,6 +22,27 @@ function getComputerChoice() {
 function updateScore() {
     playerScore_div.textContent ="Your score: " + userScore;
     computerScore_div.textContent = "Computer score: " + computerScore
+}
+
+function resetGame(){
+        userScore = 0
+        computerScore = 0
+        roundsPlayed = 0
+        updateScore()
+        showDown_div.textContent = "Game reset! Lets play again :)"
+}
+
+function checkWinner() {
+    if (roundsPlayed === maxRounds) {
+        if (userScore > computerScore) {
+            showDown_div.textContent = "You win the game!";
+        } else if (userScore < computerScore) {
+            showDown_div.textContent = "You lose the game :(";
+        } else {
+            showDown_div.textContent = "DRAW";
+        }
+        resetGame();
+    }
 }
 /* while loop necessary to instate a full game
 so while (i < = 7) {game continues with}
@@ -46,6 +69,8 @@ rockChoice.addEventListener('click', (evt) => {
         showDown_div.textContent = "Computer chose rock...Its a draw :/"
     }
     updateScore()
+    roundsPlayed ++
+    checkWinner()
 })
 
 paperChoice.addEventListener('click', (evt) => {
@@ -66,27 +91,36 @@ paperChoice.addEventListener('click', (evt) => {
         userScore++
     }
     updateScore()
+    roundsPlayed ++
+    checkWinner()
 })
 
 scissorsChoice.addEventListener('click', (evt) => {
     computerChoice = getComputerChoice();
     userChoice = scissorsChoice;
+    showDown_div.textContent = "You chose scissors."
     console.log("You chose scissors.")
     if (computerChoice === userChoice) {
         console.log("Computer chose scissors...Its a draw :/")
+        showDown_div.textContent = "Computer chose scissors...Its a draw :/"
     } else if (computerChoice === rockChoice) {
         console.log("Computer chose rock. You lose :(")
+        showDown_div.textContent = "Computer chose rock. You lose :("
         computerScore++
     } else if (computerChoice === paperChoice) {
         console.log("Computer chose paper. You win!")
+        showDown_div.textContent = "Computer chose paper. You win!"
         userScore++
     }
     updateScore()
+    roundsPlayed++
+    checkWinner()
 })
 
 resetButton.addEventListener('click', (evt) => {
-        userScore = 0
-        computerScore = 0
-        updateScore()
-        showDown_div.textContent = ""
+    userScore = 0
+    computerScore = 0
+    roundsPlayed = 0
+    updateScore()
+    showDown_div.textContent = "Game reset!"
 })
