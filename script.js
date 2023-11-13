7890723a4b322bbb0317fd8b3eac9b8c346d9c7c
Rabsickles91 <grewalrabian@gmail.com>
2023-11-13 10:41:22 -0500

    seventh commit

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 alert("BANANA")
 
 console.log("hello world")
+let userScore = 0;
+let computerScore = 0;
+let playerScore_div = document.getElementById("#playerScore")
+let computerScore_div = document.getElementById("#computerScore")
+let scoreboard_div = document.getElementsByClassName(".scoreboard")
 let rockChoice = document.querySelector("#rock")
 let paperChoice = document.querySelector("#paper")
 let scissorsChoice = document.querySelector("#scissors")
-let userChoice;
+let showDown_div = document.querySelector(".showdown")
+let userChoice; 
 let computerChoice;
 
+function getComputerChoice() {
+    let choices = [paperChoice, scissorsChoice, rockChoice]
+    let randomize = Math.floor(Math.random()*3)
+    return choices[randomize]
+}
 
 rockChoice.addEventListener('click', (evt) => {
+    computerChoice = getComputerChoice
     console.log("You chose rock.")
     if (computerChoice == scissorsChoice) {
         console.log("Computer chose scissors. You win!")
+        showDown_div.textcontent = "Computer chose scissors. You win!"
     } else if (computerChoice == paperChoice) {
         console.log("Computer chose paper. You lose :(")
+        showDown_div.textcontent = "Computer chose paper. You lose :("
     } else if (userChoice === computerChoice){
         console.log("Computer chose rock...Its a draw :/")
+        showDown_div.textcontent = "Computer chose rock...Its a draw :/"
     }
 })
 
